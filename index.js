0b70c01b6c706a3e35f863feaffc3a0b06b91c96
sourabh-yalagod <sourabhofficial99804@gmail.com>
2025-12-03 17:06:10 +0530

    some code

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-console.log("Main");
-console.log("Branch-1 with some features");
-console.log("Branch-2 with some features");
-console.log("So much of work");
+// console.log("Main");
+// console.log("Branch-1 with some features | Enhenced");
+// console.log("Branch-2 with some features");
+// console.log("So much of work");
+
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Done");
+    }, 3000)
+})
+
+async function demo() {
+    console.log("Start");
+    console.log("Response : ", await p);
+    console.log("end");
+}
+demo();
